708f067705be56f59002384223d771a1f7cdde92
dzq <x_mitsui@163.com>
2022-02-10 15:41:54 +0800

    静态资源引用:1.shop下的css为public下静态资源;2.添加了express.static(...)中间件

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false })); //yarn add body-parser
 app.use("/admin", adiminRoutes); //测试：http://localhost:3000/admin/add-product
 
 app.use(shopRoutes);
-
+app.use(express.static(path.join(__dirname, "..", "public")));
 // 处理404
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "..", "views", "404.html"));
